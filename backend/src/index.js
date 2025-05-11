5b06474b1133f15a54754c5653fbb560fe696841
hus7ain-tech <ansarihusain771@gmail.com>
2025-05-12 01:34:38 +0530

    SignUp Page completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,19 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
