5cee40f323e29100a9ce886875a242b20df8a528
hus7ain-tech <ansarihusain771@gmail.com>
2025-05-31 15:32:14 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,9 @@ import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -20,6 +23,14 @@ app.use(
 const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("App is running on port 5001");
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,8 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import { io } from "socket.io-client";
 import toast from "react-hot-toast";
-const BASE_URL = "http://localhost:5001";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 import { useChatStore } from "./useChatStore";
 
 export const useAuthStore = create((set, get) => ({
