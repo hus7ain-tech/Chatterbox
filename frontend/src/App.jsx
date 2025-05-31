254ede608771bd47b59cf04fb41639a367b8aad6
hus7ain-tech <ansarihusain771@gmail.com>
2025-05-31 15:47:43 +0530

    Project Completion, Final changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
